89aa8019f033cfa0157d73a14019a8b82442f993
Chris <microsoft@chriss-mbp.guest.corp.microsoft.com>
2017-12-12 11:02:22 -0800

    update to gulp commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,43 @@
+'use strict';
+
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var iconfont = require('gulp-iconfont');
 var iconfontCss = require('gulp-iconfont-css');
+var runSequence = require('run-sequence');
 
-var fontName = 'VSCOM Icons';
+//Create Fonts & Font SASS
+var fontName = 'vscom-icons';
 
-gulp.task('iconfont', function(){
-  gulp.src(['app/assets/icons/*.svg'])
-    .pipe(iconfontCss({
-      fontName: fontName,
-      path: 'app/assets/css/templates/_icons.css',
-      targetPath: '../../dist/css/_icons.css',
-      fontPath: '../../fonts/'
-    }))
-    .pipe(iconfont({
-      fontName: fontName,
-      prependUnicode: false,
-      formats: ['ttf', 'eot', 'woff','woff2','svg']
-     }))
-    .pipe(gulp.dest('dist/fonts/'));
+gulp.task('iconfont', function () {
+    gulp.src(['assets/icons/*.svg'])
+        .pipe(iconfontCss({
+            fontName: fontName,
+            path: 'assets/css/templates/_icons.scss',
+            targetPath: '../../assets/sass/vscom-icon.scss',
+            fontPath: '../fonts/'
+        }))
+        .pipe(iconfont({
+            fontName: fontName,
+            prependUnicode: false,
+            formats: ['ttf', 'eot', 'woff', 'woff2', 'svg']
+        }))
+        .pipe(gulp.dest('distribute/fonts/'));
+});
+
+//Compile Font SASS into CSS
+gulp.task('styles', function() {
+    return gulp.src('distribute/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./distribute/css/'));
+});
+
+//Run Sequence of Font & Sass tasks
+gulp.task('build', function (cb) {
+    runSequence(['iconfont', 'styles'], cb);
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('assets/sass/**/*.scss',['styles']);
 });
