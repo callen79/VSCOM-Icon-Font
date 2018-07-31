41f41415780db22ea5844117d984cb07250914ff
Chris <microsoft@Chriss-MBP.guest.corp.microsoft.com>
2018-07-31 11:39:33 -0700

    Renamed all svg icons to hyphen case, removed redundant icons and logos

--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,19 @@ var ttf2svg = require('gulp-ttf-svg');
 var fs = require('fs');
 var fontBlast = require('font-blast');
 
-//Convert TTF font type (MLD2 Icon font) into single SVG file
+//Convert TTF font type (MLD2 Icon font or subset) into single SVG file
 gulp.task('ttf2svg', function () {
     return gulp.src('src/fonts/**.ttf')
         .pipe(ttf2svg())
         .pipe(gulp.dest('src/svg'));
 });
 
-//Extract glhpys fron SVG font (mld2 icon font converted from ttf2svg task above) into individual SVG files
+//Extract glyphs fron single SVG font (mld2 icon font converted from ttf2svg task above task) into individual SVG files
 gulp.task('font-blast', function () {
     return gulp.src('src/svg/vscom-icons.svg')
         .pipe(fontBlast('src/svg/vscom-icons.svg', 'src/icons'));
 });
 
-
-// This will run in this order:
-// * iconfont
-// * styles
-// * Finally call the callback function
-
-gulp.task('convert', function (done) {
-    runSequence('ttf2svg', 'blast', function () {
-        console.log('Run something else');
-        done();
-    });
-});
 
 //Create Fonts & SASS template
 var fontName = 'vscom-icons';
@@ -51,7 +39,7 @@ gulp.task('iconfont', function () {
         .pipe(iconfont({
             fontName: fontName,
             prependUnicode: false,
-            formats: ['ttf', 'woff', 'woff2']
+            formats: ['ttf', 'woff', 'woff2','svg']
         }))
         .pipe(gulp.dest('dist/fonts/'));
 });
