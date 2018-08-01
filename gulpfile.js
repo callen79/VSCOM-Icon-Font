f0d257c04c739a40a556046d4b9de0e702589f87
Chris Allen <microsoft@Chriss-MBP.guest.corp.microsoft.com>
2018-08-01 16:24:22 -0700

    Added 6 third party logos to the set

--- gulpfile.js
+++ gulpfile.js
@@ -8,24 +8,25 @@ var ttf2svg = require('gulp-ttf-svg');
 var fs = require('fs');
 var fontBlast = require('font-blast');
 
-//Convert TTF font type (MLD2 Icon font or subset) into single SVG file
+//Convert TTF font type (MLD2 Icon font or subset) into SVG font
+     //Only use if new MLD2 icons need to be added - only ttf available
 gulp.task('ttf2svg', function () {
     return gulp.src('src/fonts/**.ttf')
         .pipe(ttf2svg())
-        .pipe(gulp.dest('src/svg'));
+        .pipe(gulp.dest('src/fonts/svg'));
 });
 
-//Extract glyphs fron single SVG font (mld2 icon font converted from ttf2svg task above task) into individual SVG files
+//Extract glyphs from SVG font and separate into individual SVG files
+    //Use with the ttf2svg task above
 gulp.task('font-blast', function () {
-    return gulp.src('src/svg/vscom-icons.svg')
-        .pipe(fontBlast('src/svg/vscom-icons.svg', 'src/icons'));
+    return gulp.src('src/fonts/svg/vscom-icons.svg')
+        .pipe(fontBlast('src/fonts/fonts/svg/vscom-icons.svg', 'src/icons'));
 });
 
-
-//Create Fonts & SASS template
+//Create Fonts & SASS template from individual SVG icons (src/icons)
 var fontName = 'vscom-icons';
 
-gulp.task('iconfont', function () {
+gulp.task('create-font', function () {
     gulp.src(['src/icons/svg/*.svg'], { base: 'src/' })
         .pipe(iconfontCss({
             fontName: fontName,
